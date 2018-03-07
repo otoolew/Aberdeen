43109b8e53d812bff096a5001e49b3405de2ff94
Bill O'Toole <otoolew@gmail.com>
2018-03-06 20:34:17 -0500

    Scriptable Audio and Game Events

---
+++ Assets/Code/Core/Audio/BasicAudioEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+using System;
+
+[CreateAssetMenu(menuName = "Audio Events/Basic")]
+public class BasicAudioEvent : AudioEvent {
+
+    public AudioClip[] clips;
+
+    public RangedFloat volume;
+
+    [MinMaxRange(0, 2)]
+    public RangedFloat pitch;
+
+    public override void Play(AudioSource source)
+    {
+        if (clips.Length == 0) return;
+
+        source.clip = clips[Random.Range(0, clips.Length)];
+        source.volume = Random.Range(volume.minValue, volume.maxValue);
+        source.pitch = Random.Range(pitch.minValue, pitch.maxValue);
+        source.Play();
+    }
+}
---
+++ Assets/Code/Game/AudioEventBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEventBehavior : MonoBehaviour {
+    public AudioEvent m_ExplosionAudioEvent;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
