889169f83fc06fde79080aced9d58155c415b261
otoolew <otoolew@gmail.com>
2018-03-12 17:10:07 -0400

    ExperimentalSO work

---
+++ Assets/Code/ExperimentalSO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class ExperimentalSO : ScriptableObject {
+    public int ID;
+    public int HP;
+
+#if UNITY_EDITOR
+    [Multiline]
+    public string DeveloperDescription = "";
+#endif
+    public UnityEvent particleEvent;
+
+
+    public void ShoutOut(ExperimentalTrigger et)
+    {
+        et.ShoutOut();   
+    }
+    public void EventFire()
+    {
+        particleEvent.Invoke();
+    }
+    
+}
