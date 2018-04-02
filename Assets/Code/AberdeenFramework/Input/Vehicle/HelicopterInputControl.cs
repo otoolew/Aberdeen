5135651dfd0ffbcd6bd7ec3489a8ac68ad185703
otoolew <otoolew@gmail.com>
2018-04-02 15:28:00 -0400

    Helicopter Control implementation

--- Assets/Code/AberdeenFramework/Input/Vehicle/HelicopterInputControl.cs
+++ Assets/Code/AberdeenFramework/Input/Vehicle/HelicopterInputControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class HelicopterInputControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
